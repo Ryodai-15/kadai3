5dd0fc81130ebe84b2e385d3af6b894c4f9d8b54
EC2 Default User <ec2-user@ip-172-31-35-146.ec2.internal>
2021-07-02 09:25:26 +0000

    [start]kadai3

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  get 'home/about' => "homes#about"
+
+  root to:'homes#top'
+  devise_for :users
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+
+  end
+  resources :users, only: [:new, :create, :index, :show, :edit, :update]
+
+end
